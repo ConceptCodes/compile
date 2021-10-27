2a4ccbcc504c02e83e82807b1e17f8b4bc79e38c
Concept <conceptcodes@gmail.com>
2021-10-27 19:58:43 -0400

    content and image visible, still need to adjust css

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,32 +1,47 @@
 <template>
-  <v-card>
-    <div class="content">
-      <v-card-title v-text="title"></v-card-title>
-      <v-card-subtitle v-text="body"></v-card-subtitle>
-    </div>
-    <v-img class="d-none d-lg-block" :src="img" lazy-src="https://via.placeholder.com/500"></v-img>
+  <v-card flat color="transparent">
+    <v-row class="py-3">
+      <v-col class="content mt-5">
+        <v-card-title
+          class="display-3 font-weight-bold mb-3 text-capitalize"
+          v-text="title"
+        ></v-card-title>
+        <v-card-subtitle class="mb-4" v-text="body"></v-card-subtitle>
+        <v-btn block elevation="2" large outlined rounded>Begin</v-btn>
+      </v-col>
+      <v-col>
+        <v-img
+          class="d-none d-lg-block"
+          :src="img"
+          lazy-src="https://via.placeholder.com/500"
+        ></v-img>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
 <script>
 export default {
-    name: 'banner',
-    props: {
-        title: {
-            type: String,
-            default: 'This is a title'
-        },
-        body: {
-            type: String,
-            default: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad molestiae quae quia eos quam distinctio adipisci nostrum quaerat eius et. Maiores error aliquam vitae, dicta et assumenda animi voluptas ab.'
-        },
-        img: {
-            type: String
-        }
-    }
-}
+  name: "banner",
+  props: {
+    title: {
+      type: String,
+      default: "This is a title",
+    },
+    body: {
+      type: String,
+      default:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad molestiae quae quia eos quam distinctio adipisci nostrum quaerat eius et. Maiores error aliquam vitae, dicta et assumenda animi voluptas ab.",
+    },
+    img: {
+      type: String,
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.content {
+  z-index: 2;
+}
 </style>
