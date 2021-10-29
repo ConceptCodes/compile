3716ed0f83e9fcc2d83ea09db434a7a17a4195c6
Concept <conceptcodes@gmail.com>
2021-10-29 12:37:05 -0400

    dark navbar, still working on having it transparent and above content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <v-app>
-    <v-toolbar color="transparent" flat>
-      <v-toolbar-title class="font-weight-bold" >Compile;</v-toolbar-title>
+    <v-toolbar id="navbar" dark flat>
+      <v-toolbar-title  class="font-weight-bold">
+        <a class="white--text" href="/">Compile;</a>
+        <span>
+          <v-chip class="ma-2 text-capitalize" x-small>beta</v-chip>
+        </span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <!-- <v-btn text href="/paraphase">
-        Paraphrase
-      </v-btn>
-      <v-btn text href="/qa">
+      <v-btn text href="/paraphrase"> Paraphrase </v-btn>
+      <!--  <v-btn text href="/qa">
         Question & Answering
       </v-btn> -->
     </v-toolbar>
@@ -19,9 +22,25 @@
 <script>
 export default {
   name: "App",
+  mounted(){
+    // let colors = {
+    //   'Summary' : 'purple darken-3',
+    //   'Paraphrase': 'red accent-3'
+    // }
+    // this.toolbar_color = colors[this.$router.currentRoute.name]
+    // console.log(this.$router.currentRoute)
+  },
+  data: () => ({
+    // toolbar_color: ''
+  })
 };
 </script>
 
 <style lang="scss">
-
+a {
+  text-decoration: none;
+}
+#navbar {
+  z-index: 2;
+}
 </style>
